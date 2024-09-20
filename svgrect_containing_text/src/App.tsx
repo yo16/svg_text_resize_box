c68d97ebb1a459e4592560683d4b0f4b10ddc168
yo16 <xxyo1xx@gmail.com>
2024-09-20 23:48:41 +0900

    finished RectContainText

--- svgrect_containing_text/src/App.tsx
+++ svgrect_containing_text/src/App.tsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 
-
-import { RectContainText1 } from './components/RectContainText1';
+import { RectContainText } from './components/RectContainText';
 
 function App() {
+    const [fontSize, setFontSize] = useState<number>(24);
+    const [maxWidth, setMaxWidth] = useState<number>(400);
+    const [usingText, setUsingText] = useState<string>("test1 test2 test3 test4 test5 test6 test7 test8 test9 test10");
+
     function handleOnSetSize(width: number, height: number): void {
         console.log(`w:${width}, h:${height}`);
+    }
+
+    // フォントサイズを2ずつ上げたり下げたりする
+    function handleFontSizeButton(sizeUp: Boolean): void {
+        const nextFontSize = fontSize + (sizeUp? 2: -2);
+        if ((nextFontSize > 0) && (nextFontSize < 60)) {
+            setFontSize(nextFontSize);
+        }
+    }
+
+    // 最大幅を20ずつ上げたり下げたりする
+    function handleMaxWidthButton(sizeUp: Boolean): void {
+        const nextMaxWidth = maxWidth + (sizeUp? 20: -20);
+        if ((nextMaxWidth > 0) && (nextMaxWidth < 500)) {
+            setMaxWidth(nextMaxWidth);
+        }
     }
 
     return (
@@ -14,16 +34,76 @@ function App() {
                 height={500}
                 style={{backgroundColor: "#ccc"}}
             >
-                <RectContainText1
-                    text="test1 test2 test3 test4 test5 test6 test7 test8 test9 test10"
-                    fontSize={30}
+                <RectContainText
+                    text={usingText}
+                    fontSize={fontSize}
                     x={10}
                     y={10}
                     minWidth={50}
-                    maxWidth={400}
+                    maxWidth={maxWidth}
                     onSetSize={handleOnSetSize}
                 />
-            </svg>
+
+
+                <text
+                    x={10}
+                    y={440}
+                    stroke={"#333"}
+                    fontSize={fontSize}
+                >fontSize={fontSize}</text>
+
+                <text
+                    x={15}
+                    y={475}
+                    stroke={"#333"}
+                >
+                    maxWidth: {maxWidth}
+                </text>
+                <line
+                    x1={10}
+                    y1={480}
+                    x2={10+maxWidth}
+                    y2={480}
+                    stroke={"#333"}
+                    strokeWidth={1}
+                />
+                <line
+                    x1={10}
+                    y1={475}
+                    x2={10}
+                    y2={485}
+                    stroke={"#333"}
+                    strokeWidth={1}
+                />
+                <line
+                    x1={10}
+                    y1={0}
+                    x2={10}
+                    y2={500}
+                    stroke={"#999"}
+                    strokeWidth={1}
+                    strokeDasharray={4}
+                />
+                <line
+                    x1={10+maxWidth}
+                    y1={475}
+                    x2={10+maxWidth}
+                    y2={485}
+                    stroke={"#333"}
+                    strokeWidth={1}
+                />
+                <line
+                    x1={10+maxWidth}
+                    y1={0}
+                    x2={10+maxWidth}
+                    y2={500}
+                    stroke={"#999"}
+                    strokeWidth={1}
+                    strokeDasharray={4}
+                />
+            </svg><br />
+            fontSize <button onClick={() => handleFontSizeButton(true)}>UP</button> <button onClick={() => handleFontSizeButton(false)}>DOWN</button><br />
+            maxWidth <button onClick={() => handleMaxWidthButton(true)}>UP</button> <button onClick={() => handleMaxWidthButton(false)}>DOWN</button><br />
         </>
     )
 }
---
+++ svgrect_containing_text/src/components/RectContainText2.tsx
@@ -0,0 +1,127 @@
+import { useRef, useLayoutEffect } from "react";
+import { RectContainTextProps } from "./RectContainText"
+
+export const RectContainText2: React.FC<RectContainTextProps> = ({
+    text,
+    fontSize,
+    x,
+    y,
+    minWidth,
+    maxWidth,
+    onSetSize,
+}) => {
+    const textRef = useRef<SVGTextElement>(null);
+    const rectRef = useRef<SVGRectElement>(null);
+
+    // 描画の直前にtextを解析して、<text>、<rect>を書き換える
+    useLayoutEffect(
+        () => {
+            // text
+            let rectSize: {width: number, height: number} = {width: 0, height: 0};
+            if (textRef.current) {
+                // textをスペース区切りで配列にする
+                const words: string[] = text.split(' ');
+
+                // 折り返しの１行目（textの１つ目）にlineを入れてみて、
+                // maxWidthを超えていなければTrue、超えている場合はFalse
+                const getLineBB = (line: string): DOMRect | null => {
+                    if (textRef.current) {
+                        textRef.current.textContent = line;
+                        const bb = textRef.current.getBBox();
+                        return bb;
+                        //const lineWidth = bb.width;
+                        //return lineWidth <= maxWidth;
+                    }
+                    return null;
+                }
+
+                // maxWidthに収まるlinesを作る
+                const lines: string[] = [];
+                let currentLine = '';
+                let linesMaxWidth: number = 0;
+                words.forEach((word) => {
+                    // wordを１つ追加
+                    const testCurrentLine: string
+                        = currentLine ? `${currentLine} ${word}`: word;
+
+                    // textを入れてみたBBを取得
+                    const curBB: DOMRect | null = getLineBB(testCurrentLine);
+                    if (!curBB) return;
+                    if (curBB.width <= maxWidth) {
+                        // 超えていない
+                        currentLine = testCurrentLine;
+                        linesMaxWidth = (linesMaxWidth < curBB.width)? curBB.width: linesMaxWidth;
+                    } else {
+                        // 超えている
+                        // wordを加える前をlinesに入れ、
+                        lines.push(currentLine);
+                        // 次のcurrentLineにwordを入れる
+                        currentLine = word;
+                    }
+                });
+                lines.push(currentLine);
+                const curBB: DOMRect | null = getLineBB(currentLine);
+                if (!curBB) return;
+                linesMaxWidth = (linesMaxWidth < curBB.width)? curBB.width: linesMaxWidth;
+                const oneLineHeight = curBB.height;
+
+                // textRefを直接変更
+                // 一旦すべて削除
+                textRef.current.textContent = "";
+                textRef.current.innerHTML = "";
+                // 子要素としてtspanを作成して追加
+                const svgNS = "http://www.w3.org/2000/svg";
+                lines.forEach((line) => {
+                    const tspanElement = document.createElementNS(svgNS, "tspan");
+                    tspanElement.setAttribute("x", `${x}`);
+                    tspanElement.setAttribute("dx", "0");
+                    tspanElement.setAttribute("dy", `${oneLineHeight}`);
+                    tspanElement.textContent = line;
+                    
+                    textRef.current?.appendChild(tspanElement);
+                });
+
+                // rectのために情報を渡す
+                rectSize = {
+                    width: linesMaxWidth,
+                    height: oneLineHeight * lines.length,
+                };
+            }
+
+            // rect
+            if (rectRef.current) {
+                const newWidth = rectSize.width;
+                const newHeight = rectSize.height;
+
+                rectRef.current.setAttribute("width", `${newWidth}`);
+                rectRef.current.setAttribute("height", `${newHeight}`);
+
+                onSetSize(newWidth, newHeight);
+            }
+        },
+        [text, fontSize, minWidth, maxWidth]
+    );
+
+    return (
+        <g>
+            <rect
+                ref={rectRef}
+                x={x}
+                y={y}
+                width={10}
+                height={10}
+                fill="#f1dfa2"
+                stroke="#a79a70"
+                strokeWidth={1}
+            />
+            <text
+                ref={textRef}
+                x={x}
+                y={y}
+                dominantBaseline={"text-after-edge"}
+                fontSize={fontSize}
+            ></text>
+        </g>
+    );
+}
+
